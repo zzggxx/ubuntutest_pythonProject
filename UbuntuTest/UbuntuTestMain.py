178d2106e246b8d6a688b205bb4de3cc853fd6d6
Will <zhougaoxiongcall@163.com>
2021-05-11 14:28:48 +0800

    serial port

--- UbuntuTest/UbuntuTestMain.py
+++ UbuntuTest/UbuntuTestMain.py
@@ -6,9 +6,9 @@ import ubuntutest
 # 是测试4G模块的包
 import platform
 import subprocess
-# 串口测试
+# 串口测试 pip install pyserial
 import serial
-#
+# 线程安全的包Timer
 from threading import Timer
 
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
@@ -33,18 +33,53 @@ def open_Serial(baudrate, port, timeout):
     return ser, ser.is_open;
 
 
+# ttyS0:485   ttyS1 ttyS2 ttyS3:232
+mTestString = 'sdfaeoifyalfhalieyrilahlfayergjdghfkjvgJHSbfkjawetgywegrfweyrt78teyiYQuiwhwlefhluewgrfffffdhbvxhcgfuegfjsbdcjsgerytfwuehfzbjxuyctweguyfbjhxdtguyebceywtfuwgyejhasguywdfegyrfuysgdjvcuwetygfuiwgehflhaeuirtypqeyteluhdfjksdb641231231564867465135468e7f4s5d1f3a54w5r3eq4'
+
+
 def onClick_Button_Serial():
+    testStringLen = len(mTestString)
     ser0, isopen0 = open_Serial(9600, '/dev/ttySC0', 10)
     ser1, isopen1 = open_Serial(9600, '/dev/ttySC1', 10)
     if isopen0 and isopen1:
-        Timer(1, ser1.write('abcdefg')).start()
-        recv = []
-        while True:
-            if ser0.in_waiting:
-                r = ser0.read()
-                if r != None:
-                    recv.append(r)
-                    print(recv)
+        ser0.write(mTestString)
+        receivedata = ser1.read(testStringLen).decode()
+        if receivedata in mTestString:
+            ser0Write9600 = True
+            ser1Read9600 = True
+            print('----------')
+        ser1.write(mTestString)
+        receivedata = ser0.read(testStringLen).decode()
+        if receivedata in mTestString:
+            ser1Write9600 = True
+            ser0Read9600 = True
+            print('----------++++++++++')
+        ser0.close()
+        ser1.close()
+'''
+    ser0, isopen0 = open_Serial(115200, '/dev/ttySC0', 10)
+    ser1, isopen1 = open_Serial(115200, '/dev/ttySC1', 10)
+    if isopen0 and isopen1:
+        ser0.write(mTestString)
+        receivedata = ser1.read(testStringLen).decode()
+        if receivedata in mTestString:
+            ser0Write115200 = True
+            ser1Read115200 = True
+        ser1.write(mTestString)
+        receivedata = ser0.read(testStringLen).decode()
+        if receivedata in mTestString:
+            ser1Write115200 = True
+            ser0Read115200 = True
+        ser0.close()
+        ser1.close()
+
+    if ser0Write9600 and ser0Write115200 and ser0Read9600 and ser0Read115200:
+        ser0 = True
+        print('the serial port is right: %s' % ser0)
+    if ser1Write9600 and ser1Write115200 and ser1Read9600 and ser1Read115200:
+        ser1 = True
+        print('the serial port is right: %s' % ser1)
+'''
 
 
 if __name__ == '__main__':
