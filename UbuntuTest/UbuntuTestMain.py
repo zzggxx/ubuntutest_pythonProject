bfbc34f50cd3c5225949a186fd91238d418f33a7
Will <zhougaoxiongcall@163.com>
2021-05-12 13:38:02 +0800

    serial port

--- UbuntuTest/UbuntuTestMain.py
+++ UbuntuTest/UbuntuTestMain.py
@@ -34,8 +34,29 @@ def open_Serial(baudrate, port, timeout):
 
 
 # ttyS0:485   ttyS1 ttyS2 ttyS3:232
-mTestString = 'sdfaeoifyalfhalieyrilahlfayergjdghfkjvgJHSbfkjawetgywegrfweyrt78teyiYQuiwhwlefhluewgrfffffdhbvxhcgfuegfjsbdcjsgerytfwuehfzbjxuyctweguyfbjhxdtguyebceywtfuwgyejhasguywdfegyrfuysgdjvcuwetygfuiwgehflhaeuirtypqeyteluhdfjksdb641231231564867465135468e7f4s5d1f3a54w5r3eq4'
-
+mTestString = 'sdfaeoifyalfhalieyrilahlfayergjdghfkjvgJHSbfkjawetgywegrfweyrt78teyiYQuiwhwlefhluewgrfffffdhbvxhcgfuegf' \
+              'jsbdcjsgerytfwuehfzbjxuyctweguyfbjhxdtguyebceywtfuwgyejhasguywdfegyrfuysgdjvcuwetygfuiwgehflhaeuirtypqsd' \
+              'faeoifyalfhalieyrilahlfayergjdghfkjvgJHSbfkjawetgywegrfweyrt78teyiYQuiwhwlefhluewgrfffffdhbvxhcgfuegfjsbdcjs' \
+              'gerytfwuehfzbjxuyctweguyfbjhxdtguyebceywtfuwgyejhasguywdfegyrfuysgdjvcuwetygfuiwgehflhaeuirtypqeyteluhdfjksd' \
+              'b641231231564867465135468e7f4s5d1f3a54w5r3eqsdfaeoifyalfhalieyrilahlfayergjdghfkjvgJHSbfkjawetgywegrfweyrt78tey' \
+              'iYQuiwhwlefhluewgrfffffdhbvxhcgfuegfjsbdcjsgerytfwuehfzbjxuyctweguyfbjhxdtguyebceywtfuwgyejhasguywdfegyrfuysgdjvcuwe' \
+              'tygfuiwgehflhaeuirtypqeyteluhdfjksdb641231231564867465135468e7f4s5d1f3a54w5r3eq4sdfaeoifyalfhalieyrilahlfayergjdghfkjvgJHSb' \
+              'fkjawetgywegrfweyrt78teyiYQuiwhwlefhluewgrfffffdhbvxhcgfuegfjsbdcjsgerytfwuehfzbjxuyctweguyfbjhxdtguyebceywtfuwgyejhasgu' \
+              'ywdfegyrfuysgdjvcuwetygfuiwgehflhaeuirtypqeyteluhdfjksdb641231231564867465135468e7f4s5d1f3a54w5r3eq4sdfaeoifyalfhalieyrilah' \
+              'lfayergjdghfkjvgJHSbfkjawetgywegrfweyrt78teyiYQuiwhwlefhluewgrfffffdhbvxhcgfuegfjsbdcjsgerytfwuehfzbjxuyctweguy' \
+              'fbjhxdtguyebceywtfuwgyejhasguywdfegyrfuysgdjvcuwetygfuiwgehflhaeuirtypqeyteluhdfjksdb641231231564867465135468e7fsdfaeoif' \
+              'yalfhalieyrilahlfayergjdghfkjvgJHSbfkjawetgywegrfweyrt78teyiYQuiwhwlefhluewgrfffffdhbvxhcgfuegfjsbdcjsgerytfwuehfzbjxuy' \
+              'ctweguyfbjhxdtguyebceywtfuwgyejhasguywdfegyrfuysgdjvcuwetygfuiwgehflhaeuirtypqeyteluhdfjksdb641231231564867465135468e7f4s5d1' \
+              'sdfaeoifyalfhalieyrilahlfayergjdghfkjvgJHSbfkjawetgywegrfweyrt78teyiYQuiwhwlefhluewgrfffffdhbvxhcgfuegfjsbdcjsgerytfwuehfzbj' \
+              'xuyctweguyfbjhxdtguyebceywtfuwgyejhasguywdfegyrfuysgdjvcuwetygfuiwgehflhaeuirtypqeyteluhdfjksdb6sdfaeoifyalfhalieyrilahlfaye' \
+              'rgjdghfkjvgJHSbfkjawetgywegrfweyrt78teyiYQuiwhwlefhluewgrfffffdhbvxhcgfuegfjsbdcjsgerytfwuehfzbjxuyctweguyfbjhxdtguyebceywtf' \
+              'uwgyejhasguywdfegyrfuysgdjvcuwetygfuiwgehflhaeuirtypqeyteluhdfjksdb64123123156sdfaeoifyalfhalieyrilahlfayergjdghfkjvgJHSbfkj' \
+              'awetgywegrfweyrt78teyiYQuiwhwlefhluewgrfffffdhbvxhcgfuegfjsbdcjsgerytfwuehfzbjxuyctweguyfbjhxdtguyebceywtfuwgyejhasguywdfegyr' \
+              'fuysgdjvcuwetygfuiwgehflhaeuirtypqeyteluhdfjksdb6412312sdfaeoifyalfhalieyrilahlfayergjdghfkjvgJHSbfkjawetgywegrfweyrt78t' \
+              'eyiYQuiwhwlefhluewgrfffffdhbvxhcgfuegfjsbdcjsgerytfwuehfzbjxuyctweguyfbjhxdtguyebceywtfuwgyejhasguywdfegyrfuysgdjvcuwetygf' \
+              'uiwgehflhaeuirtypqeyteluhdfjksdb641231231564867465135468e7f4s5d1f3a54w5r3eq431564867465135468e7f4s5d1f3a54w5r3eq44867465135' \
+              '468e7f4s5d1f3a54w5r3eq441231231564867465135468e7f4s5d1f3a54w5r3eq4f3a54w5r3eq44s5d1f3a54w5r3eq44eyteluhdfjksdb64123123156486' \
+              '7465135468e7f4s5d1f3a54w5r3eq4'
 
 def onClick_Button_Serial():
     testStringLen = len(mTestString)
@@ -63,33 +84,33 @@ def onClick_Button_Serial():
     ser2 = False
     ser3 = False
 
-    ser0, isopen0 = open_Serial(9600, '/dev/ttyS0', 10)
-    ser1, isopen1 = open_Serial(9600, '/dev/ttyS1', 10)
+    ser0, isopen0 = open_Serial(9600, '/dev/ttyS0', 5)
+    ser1, isopen1 = open_Serial(9600, '/dev/ttyS1', 5)
     if isopen0 and isopen1:
         ser0.write(mTestString.encode())
         receivedata10 = ser1.read(testStringLen).decode()
-        if receivedata10 in mTestString:
+        if mTestString in receivedata10:
             ser0Write9600 = True
             ser1Read9600 = True
         ser1.write(mTestString.encode())
         receivedata00 = ser0.read(testStringLen).decode()
-        if (receivedata00 in mTestString):
+        if mTestString in receivedata00:
             ser1Write9600 = True
             ser0Read9600 = True
         ser0.close()
         ser1.close()
 
-    ser0, isopen0 = open_Serial(115200, '/dev/ttyS0', 10)
-    ser1, isopen1 = open_Serial(115200, '/dev/ttyS1', 10)
+    ser0, isopen0 = open_Serial(115200, '/dev/ttyS0', 5)
+    ser1, isopen1 = open_Serial(115200, '/dev/ttyS1', 5)
     if isopen0 and isopen1:
         ser0.write(mTestString.encode())
         receivedata11 = ser1.read(testStringLen).decode()
-        if receivedata11 in mTestString:
+        if mTestString in receivedata11:
             ser0Write115200 = True
             ser1Read115200 = True
         ser1.write(mTestString.encode())
         receivedata01 = ser0.read(testStringLen).decode()
-        if receivedata01 in mTestString:
+        if mTestString in receivedata01:
             ser1Write115200 = True
             ser0Read115200 = True
         ser0.close()
@@ -102,33 +123,33 @@ def onClick_Button_Serial():
         ser1 = True
         print('the serial1 port is right: %s' % ser1)
 
-    ser2, isopen2 = open_Serial(9600, '/dev/ttyS2', 10)
-    ser3, isopen3 = open_Serial(9600, '/dev/ttyS3', 10)
+    ser2, isopen2 = open_Serial(9600, '/dev/ttyS2', 5)
+    ser3, isopen3 = open_Serial(9600, '/dev/ttyS3', 5)
     if isopen2 and isopen3:
         ser2.write(mTestString.encode())
         receivedata30 = ser3.read(testStringLen).decode()
-        if receivedata30 in mTestString:
+        if mTestString in receivedata30:
             ser2Write9600 = True
             ser3Read9600 = True
         ser3.write(mTestString.encode())
         receivedata20 = ser2.read(testStringLen).decode()
-        if (receivedata20 in mTestString):
+        if mTestString in receivedata20:
             ser3Write9600 = True
             ser2Read9600 = True
         ser2.close()
         ser3.close()
 
-    ser2, isopen2 = open_Serial(115200, '/dev/ttyS2', 10)
-    ser3, isopen3 = open_Serial(115200, '/dev/ttyS3', 10)
+    ser2, isopen2 = open_Serial(115200, '/dev/ttyS2', 5)
+    ser3, isopen3 = open_Serial(115200, '/dev/ttyS3', 5)
     if isopen2 and isopen3:
         ser2.write(mTestString.encode())
         receivedata31 = ser3.read(testStringLen).decode()
-        if receivedata31 in mTestString:
+        if mTestString in receivedata31:
             ser2Write115200 = True
             ser3Read115200 = True
         ser3.write(mTestString.encode())
         receivedata21 = ser2.read(testStringLen).decode()
-        if receivedata21 in mTestString:
+        if mTestString in receivedata21:
             ser3Write115200 = True
             ser2Read115200 = True
         ser2.close()
