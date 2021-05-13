0ea1a3907b4549f9c4fcacac8f443e3e15af7c3d
Will <zhougaoxiongcall@163.com>
2021-05-13 16:41:04 +0800

    add basic test , study

--- pythbasicproject/testmain.py
+++ pythbasicproject/testmain.py
@@ -14,12 +14,12 @@ from snbc.snbc import *
 print(add(1, 2))
 print(addd(1, 2))
 '''
-
+'''
 # if __name__ == '__main__':
 result = test.add(1, 2)
 print(result)
 result = test.addd(1, 2)
 print(result)
-
+'''
 # 编辑的时候就错误,因为没有导入
 # test2.
